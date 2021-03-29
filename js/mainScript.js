dc53941be3dc195492af0fcb58dc841dd99b25b4
Bilikto <dashievbilikto@gmail.com>
2021-03-29 11:50:49 -0400

    imported files

---
+++ js/mainScript.js
@@ -0,0 +1,19 @@
+import tabs from './modules/tabs.js';
+import timer from './modules/timer.js';
+import modal from './modules/modal.js';
+import form from './modules/form.js';
+import slides from './modules/slides.js';
+import calculator from './modules/calculator.js';
+
+
+window.addEventListener('DOMContentLoaded', () => {
+	'use strict';
+
+	tabs();
+	timer();
+	modal();
+	form();
+	slides();
+	calculator();
+
+});
