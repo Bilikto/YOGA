f348fbd2985d6b0bba843fa150b39387aa993161
Bilikto <dashievbilikto@gmail.com>
2021-03-29 11:51:39 -0400

    created postData func

---
+++ js/services/services.js
@@ -0,0 +1,27 @@
+function postData(data) {
+  return new Promise((resolve, reject) => {
+    const request = new XMLHttpRequest();
+
+    request.open('POST', 'server.php');
+
+    request.setRequestHeader('Content-type', 'application/json;charset=UTF-8');
+  
+    request.send(data);
+
+    request.addEventListener('load', () => {
+      if (request.status == 200) {
+        console.log("Done");
+        resolve();
+      } else {
+        console.log("Request failed..");
+        throw new Error(`new Error ${request.status}: ${request.statusText}`);
+      }
+    });
+
+    request.addEventListener('error', () => {
+     reject();
+    });
+  });
+}
+
+export default postData;
